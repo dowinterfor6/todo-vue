0ba4fe80cd49113675514183be79603ac79a8b49
Andrew Chan <achan@nisum.com>
2020-02-03 11:36:25 -0800

    Add basic styling to input bars

--- src/components/InputFields.vue
+++ src/components/InputFields.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>
-    <input
-      type="text"
-      class="todo-input"
-      placeholder="What needs to be done"
-      v-model="newTodo"
-      @keyup.enter="willAddTodo"
-    >
+  <div class="input-fields-container">
+      <i
+        class="fas fa-plus input"
+        @click="toggleState($event.target.classList[2].toUpperCase())"
+      >
+      </i>
+      <transition name="input-fields" mode="out-in">
+        <input
+          type="text"
+          class="todo-input"
+          key="INPUT"
+          placeholder="What needs to be done"
+          v-model="newTodo"
+          @keyup.enter="willAddTodo"
+          v-if="state === 'INPUT'"
+        >
+        <input
+          type="text"
+          class="todo-search"
+          key="SEARCH"
+          placeholder="Look for a todo"
+          v-model="searchTerm"
+          @keyup.enter="willAddTodo"
+          v-if="state === 'SEARCH'"
+        >
+      </transition>
+      <i
+        class="fas fa-search search"
+        @click="toggleState($event.target.classList[2].toUpperCase())"
+      >
+      </i>
   </div>
 </template>
 
@@ -17,27 +40,66 @@ export default {
   name: 'input-fields',
   data () {
     return {
-      newTodo: ''
+      newTodo: '',
+      searchTerm: '',
+      state: 'INPUT',
+      searchComponent: document.getElementById('search-container'),
+      inputComponent: document.getElementById('input-container')
     }
   },
   methods: {
     willAddTodo () {
       EventBus.$emit('addTodo', this.newTodo)
       this.newTodo = ''
+    },
+    toggleState (state) {
+      if (state !== this.state) {
+        this.state = state
+      }
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-  .todo-input {
+
+.input-fields-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  border: 1px solid red;
+
+  .todo-input, .todo-search {
+    border: 1px solid green;
     width: 100%;
-    padding: 10px 18px;
+    padding: 10px;
     font-size: 10px;
-    margin-bottom: 16px;
+    // border: none;
+    margin: 0 5px;
 
     &:focus {
       outline: 0;
     }
   }
+
+  .input-fields-enter-active,
+  .input-fields-leave-active
+  {
+    transition: opacity 0.25s ease;
+  }
+
+  .input-fields-enter,
+  .input-fields-leave-to
+  {
+    opacity: 0;
+  }
+
+  .fas {
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+
 </style>
